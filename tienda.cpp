4c4df7bf63e4346e599208937c432596ad4e4ddc
Sebatian Luis Enrique Aguilar Diaz <sebastian.luis.aguilar.diaz@gmail.com>
2026-02-04 12:32:10 -0600

    First commit

---
+++ tienda.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cmath>
+#include<random>
+#include<time.h>
+#include<chrono>
+#include<string.h>
+#include<set>
+#include <algorithm>
+
+using namespace std;
+
+// Title:     			  Store
+// Author(s):  			  Sebastian Aguilar
+// Creation Date:		  04/02/2026
+// Last update:			  04/02/2026
+// Version:				  1.0.0
+
+struct vegetable{
+
+};
+
+struct 
+
+
+
+int main(){
+    
+
+
+
+
+    return 0;
+}
